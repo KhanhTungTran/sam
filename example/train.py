c18147cfaa1e7add26ed2b92e1f0902c843fc565
TungKhanhTran <tung.tran.1805@hcmut.edu.vn>
2020-12-30 17:35:12 +0700

    Updates...

--- example/train.py
+++ example/train.py
@@ -3,7 +3,7 @@ import torch
 
 from model.wide_res_net import WideResNet
 from model.smooth_cross_entropy import smooth_crossentropy
-from data.cifar import Cifar
+from data.rest import Rest
 from utility.log import Log
 from utility.initialize import initialize
 from utility.step_lr import StepLR
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     initialize(args, seed=42)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    dataset = Cifar(args.batch_size, args.threads)
+    dataset = Rest(args.batch_size, args.threads)
     log = Log(log_each=10)
     model = WideResNet(args.depth, args.width_factor, args.dropout, in_channels=3, labels=7).to(device)
 
